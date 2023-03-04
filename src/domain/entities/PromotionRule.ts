61c36d7189cf92016957ba2ea70692746a817958
Mohamed Gnedy <mognedy@gmail.com>
2023-03-04 20:40:36 +0100

    add readonly string id to PromotionRule interface

--- src/domain/entities/PromotionRule.ts
+++ src/domain/entities/PromotionRule.ts
@@ -1,6 +1,7 @@
 import { Cart } from '../Cart';
 
 export interface PromotionRule {
+  readonly id: string;
   readonly name: string;
   canApply(cart: Cart): boolean;
   apply: (cart: Cart) => void;
